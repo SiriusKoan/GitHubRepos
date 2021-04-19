8c388bb71b0ee1e97c74d41104d6cde165a9f67a
SiriusKoan <k.peihsun@gmail.com>
2021-04-19 11:35:54 +0800

    successfully get all repos

--- popup.js
+++ popup.js
@@ -1,18 +1,28 @@
 function init() {
     chrome.storage.sync.get(["TOKEN"], function(data){
         var TOKEN = data["TOKEN"]
-        if (TOKEN == undefined) {
-            var input = document.createElement("input");
-            input.setAttribute("id", "token_field");
-            document.body.appendChild(input);
-            var btn = document.createElement("button");
-            btn.setAttribute("id", "token_input");
-            btn.innerText = "Enter";
-            document.body.appendChild(btn);
-            btn.addEventListener("click", set_token);
+        if (TOKEN) {
+            var msg = document.createElement("p");
+            msg.innerText = "loading...";
+            document.body.appendChild(msg);
+
+            var request = new XMLHttpRequest();
+            request.open("GET", "https://api.github.com/user/repos?per_page=100&visibility=all&sort=updated");
+            request.onreadystatechange = function() {
+                var repos = JSON.parse(request.responseText);
+                console.log(request.responseText);
+                render_repos(repos);
+            }
+            request.setRequestHeader("Authorization", "token "+TOKEN);
+            request.send();
         }
+        var btn = document.createElement("button");
+        btn.innerText = "setting";
+        document.body.appendChild(btn);
+        btn.addEventListener("click", function() {chrome.tabs.create({"url": "option.html"})});
     })
 }
+
 window.onload = init();
 
 function set_token() {
@@ -20,4 +30,8 @@ function set_token() {
     var TOKEN = input.value;
     chrome.storage.sync.set({"TOKEN": TOKEN});
     input.value = "OK";
+}
+
+function render_repos(repos) {
+    
 }
